d12d6f8f3b495caff772028b1701b66f7a13c82e
jaredtrh <jaredtrh20@gmail.com>
2022-10-06 21:01:56 +0800

    test

--- gotogro_mrm_db.sql
+++ gotogro_mrm_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 17, 2021 at 06:56 PM
+-- Generation Time: Oct 1, 2022 at 06:56 PM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.8
 
